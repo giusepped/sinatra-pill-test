df565a0e175e23a6cada810490beb29b634f591d
Emily Carson <emilyjanecarson@gmail.com>
2015-08-03 13:44:51 +0100

    Added hello page with pug picture

--- hello.rb
+++ hello.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/' do
   'hello'
 end
+
+get '/secret' do
+	'This is a secret page'
+end
+
+get '/hello' do
+	"<div style= 'border: 3px dashed green'> <img src= https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Sad-pug.jpg/200px-Sad-pug.jpg > </div>"
+end
